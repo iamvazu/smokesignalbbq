edf3d440618b30deecdb5e647c5338a6bd855098
iamvazu <iamvazu@gmail.com>
2026-02-19 16:09:18 +0530

    fix: import missing Button component in HowItWorksPage

--- pages/HowItWorksPage.tsx
+++ pages/HowItWorksPage.tsx
@@ -14,6 +14,7 @@ import {
     Star
 } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
+import { Button } from '../components/Button';
 
 const HowItWorksPage: React.FC = () => {
     const navigate = useNavigate();
